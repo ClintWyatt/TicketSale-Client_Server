b6ff56ad5bd1a6c7c071e63cadb5848cfafca520
Clint Wyatt <clintwyatt7@gmail.com>
2021-04-18 23:54:34 -0500

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -6,7 +6,7 @@ int main()
 	boost::asio::io_service io_service;
 	tcp::endpoint endpoint(tcp::v4(), 10000);
 	TicketServer server(io_service, endpoint);
-	std::cout << "Awaiting clients to buy tiekcts \n";
+	std::cout << "Awaiting clients to buy tickets \n";
 	io_service.run();
 	return 0;
 }
