bcda7b7b63e7dd63eddadbe93a53af081e421862
Clint Wyatt <clintwyatt7@gmail.com>
2021-04-18 23:56:28 -0500

    Update clientSockets.h

--- server/clientSockets.h
+++ server/clientSockets.h
@@ -3,6 +3,10 @@
 #include <vector>
 #include <iterator>
 #include <set>
+#include <utility>
+#include <algorithm>
+
+typedef std::pair<unsigned short, char[16]> port_ip; 
 
 class UdpClientSockets
 {
@@ -11,7 +15,7 @@ public:
 	* Method adds a socket to the set of sockets
 	* The return value is a socket not used by anyother client
 	*/
-	void addUdpSocket(unsigned short portNum);
+	void addClientInfo(unsigned short portNum, char ipAddress[]);
 
 	/*
 	* removes udp socket from a client that is leaving the ticket booth
@@ -21,14 +25,14 @@ public:
 	/*
 	* Method returns a udp socket of another client if the number of clinets buying tickets is greater than 2.
 	*/
-	unsigned short getOtherUdpSocket(unsigned short x);
+	void otherClientInfo(port_ip &obj);
 
 	/*
 	* Method returns the number of udp sockets (represents other clients)
 	*/
 	unsigned short getNumSockets();
 private:
-	std::set<unsigned short> cliSockets;
+	std::set<port_ip> cliSockets;
 	unsigned short udpSockNum = 10000;
 };
 #endif
