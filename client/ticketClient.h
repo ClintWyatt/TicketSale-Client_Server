d2b6695c438c3aeaae13e077d1a84e0cc1c723c1
Clint Wyatt <clintwyatt7@gmail.com>
2021-04-18 23:50:09 -0500

    Update ticketClient.h

--- client/ticketClient.h
+++ client/ticketClient.h
@@ -13,6 +13,7 @@
 #include <queue>
 #include <thread>
 #include <iterator>
+#include <boost/asio/signal_set.hpp>
 
 using boost::asio::ip::tcp;
 using boost::asio::ip::udp;
@@ -20,8 +21,9 @@ using boost::asio::ip::udp;
 class TicketClient
 {
 public:
-	TicketClient(boost::asio::io_service &io_service_, boost::asio::ip::tcp::resolver::iterator it);
+	TicketClient(boost::asio::io_service &io_service_, char* hostIp, char* portNum, char* ownIp);
 	~TicketClient();
+	void endUdpServer();
 	void handle_connect(tcp::resolver::results_type::iterator endpoint_iter);
 	void udpServer();
 	//boost::array <char, 64> message;
@@ -31,14 +33,21 @@ public:
 	* method takes in the portnumber for the other client and returns either a ticket price from another client or
 	* returns 0 (buy from other client failed).
 	*/
-	unsigned int buyFromScalper(unsigned short portNum, unsigned short balance, std::string ipv4);
+	unsigned int buyFromScalper(unsigned short portNum, unsigned short balance, char ipv4[]);
 
 	/*
 	* Method attempts to sell a ticket to another client
 	* Returns either the ticket that the client can buy or zero (other client does not have sufficient funds).
 	*/
 	unsigned short sellTicket(unsigned short balance);
+
+	/*
+	* Method returns a ticket to sell to the server
+	*/
+	unsigned short sellTicketToSvr();
 private:
+
+	boost::asio::deadline_timer timer;
 	void getTicket();
 	void read();
 	void getSvrResponse();
@@ -52,6 +61,8 @@ private:
 	std::queue<unsigned short> wallet;
 	int counter;
 	std::thread *t1;
+	boost::asio::signal_set signals;
+	char ownIP[20];
 };
 /*
 class UdpServer
