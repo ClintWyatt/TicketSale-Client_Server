d9548b3a02231756178f137f323e09c49c085928
Clint Wyatt <clintwyatt7@gmail.com>
2021-04-18 23:51:24 -0500

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -2,36 +2,25 @@
 #include <boost/asio.hpp>
 #include <iostream>
 using boost::asio::ip::tcp;
-std::string message;
-int main()
+
+int main(int argc, char *argv[])
 {
-	
-	boost::asio::io_service io_service;
-	boost::asio::ip::tcp::resolver resolver(io_service);
-	auto endpoint_iterator = resolver.resolve("192.168.0.183", "10000");
-	unsigned int portnum = 10000;
-	TicketClient tclient(io_service, endpoint_iterator);
-	io_service.run();
-	
-	/*
 	try
 	{
+		if (argc != 4)
+		{
+			std::cout << "Usage: <executable> <host ip> <host port> <own ip>\n";
+			return 0;
+		}
+		
 		boost::asio::io_service io_service;
-
-		tcp::resolver resolver(io_service);
-		tcp::resolver::query query("127.0.0.1", "10000");
-		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-		std::string message;
-		tcp::socket socket(io_service);
-		boost::asio::connect(socket, endpoint_iterator);
-		socket.send(boost::asio::buffer("4000"));
-		socket.receive(boost::asio::buffer(message));
-		std::cout << message << '\n';
+		TicketClient tclient(io_service, argv[1], argv[2], argv[3]);
+		io_service.run();
 	}
 	catch (boost::system::error_code ec)
 	{
-		std::cout << ec.message() << '\n';
+		std::cout << ec.message() << "\n";
 	}
-	*/
+
 	return 0;
 }
