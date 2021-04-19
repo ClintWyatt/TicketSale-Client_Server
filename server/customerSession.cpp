26f8f8a1bd509e578eee57478b040431d8e90c4a
Clint Wyatt <clintwyatt7@gmail.com>
2021-04-18 23:55:25 -0500

    Update customerSession.cpp

--- server/customerSession.cpp
+++ server/customerSession.cpp
@@ -1,5 +1,5 @@
 #include "customerSession.h"
-
+	
 CustomerSession::CustomerSession(TicketBooth& booth, UdpClientSockets& udpCli, tcp::socket socket) :
 	tckBooth(booth), socket_(std::move(socket)), cliskts(udpCli)
 {
@@ -8,7 +8,20 @@ CustomerSession::CustomerSession(TicketBooth& booth, UdpClientSockets& udpCli, t
 
 void CustomerSession::startSale()
 {
+	//shared_from_this
+	//Returns a std::shared_ptr<T> that shares ownership of *this with all 
+	//existing std::shared_ptr that refer to *this.
+
+	//Effectively executes std::shared_ptr<T>(weak_this), where weak_this is 
+	//the private mutable std::weak_ptr<T> member of enable_shared_from_this.
+	/*
+		Notes
+		It is permitted to call shared_from_this only on a previously shared object, 
+		i.e.on an object managed by std::shared_ptr(in particular, shared_from_this 
+		cannot be called during construction of * this).
+		*/
 	tckBooth.join(shared_from_this());
+	memset(ipAddress, '\0', sizeof(ipAddress));
 	rcvClientMssg();//start async operations
 }
 
@@ -38,21 +51,29 @@ void CustomerSession::rcvClientMssg()//reads the message from the client in this
 	{
 		if (!ec)
 		{
-			if (setUdpSock == false)//if the udp socket from the client is not registered yet
+			//if the udp socket from the client is not registered yet - not known to the server
+			if (setUdpSock == false)
 			{
-				char buffCpy[32];
+				char buffCpy[64];
 				memcpy(buffCpy, buff, sizeof(buff));
 				char* port;//represents the port number sent by the client
 				port = strtok(buffCpy, " \n"); //tokenizing the buffer
 				port = strtok(NULL, " \n");	//getting the port number (second integer in the buffer)
-				cliskts.addUdpSocket(atoi(port));
+				udpSkt = atoi(port);
+				port = strtok(NULL, " \n");//getting the IP address
+				memcpy(ipAddress, port, sizeof(ipAddress));
+				std::cout << "Adding client: " << udpSkt << " - " << ipAddress <<"\n";
+				cliskts.addClientInfo(udpSkt, ipAddress);
 				setUdpSock = true;
 				memset(buff, '\0', sizeof(buff));
 				memcpy(buff, buffCpy, 4);//copying only the first 4 bytes of the buffCpy, which is the client balance
+				setStatus(buff);//setting the status to the balance from the client. Tells the ticket booth that the client has money to buy a ticket.
 			}
-			setStatus(buff);//setting the status to the balance from the client. Tells the ticket booth that the client has money to buy a ticket.
+			else //client's info is known by the server
+			{
+				setStatus(buff);//setting the status to the balance from the client. Tells the ticket booth that the client has money to buy a ticket.
+			}
 			tckBooth.decideResponse(buff);//see if the client can buy a ticket from the ticket booth
-			
 		}
 		else//client likely closed the socket
 		{
@@ -77,11 +98,13 @@ void CustomerSession::sendResponse(std::string response_)
 			if (!ec)
 			{
 				std::cout << "[SERVER] SELL " << buff << " successful! \n";
+				rcvClientMssg();
 			}
-			});
-		rcvClientMssg();
+		});
+		
 	}
-	else if (response_ == "No!")//the clinet cannot buy the ticket from server
+	//the clinet cannot buy the ticket from server
+	else if (response_ == "No!")
 	{
 		recieveResponse("No!");
 	}
@@ -91,21 +114,26 @@ void CustomerSession::recieveResponse(std::string response_)
 {
 	//do synchronous operations here
 	memset(buff, '\0', sizeof(buff));
-	memcpy(buff, "No!\0", 5);
 	auto self(shared_from_this());
-	//need to insert the code to determine whether the client can buy from other clients via udp
+	
+	//if there is more than 1 client buying from the ticket booth
 	if (cliskts.getNumSockets() > 1)
 	{
+		port_ip otherCli;
+		cliskts.otherClientInfo(otherCli);//get the udp socket from amother client
+		sprintf_s(buff, sizeof(buff), "%d %s", otherCli.first, otherCli.second);
 		boost::asio::write(socket_, boost::asio::buffer(buff));//synchronous operation writing to the client
 	}
+	//only one client at the ticket booth
 	else
 	{
-		boost::asio::write(socket_, boost::asio::buffer(buff));//synchronous operation writing to the client
+		memcpy(buff, "BUY FAILED\0", 11);
+		boost::asio::write(socket_, boost::asio::buffer(buff));//snchronous operation writing to the client
 	}
 
 	boost::asio::read(socket_, boost::asio::buffer(buff));//synchronous operation specifying whether the client bought from another client or failed
 	//to get a ticket from another client
-	if (memcmp(buff, "BUY FAILED", sizeof("BUY FAILED")) != 0)//if the client could not buy a ticket from another client
+	if (memcmp(buff, "BUY SUCCESS", sizeof("BUY SUCCESS")) != 0)//if the client could not buy a ticket from another client
 	{
 		unsigned short price = atoi(buff);
 		std::cout << "[SERVER] Buying back ticktet " << tckBooth.getCliTicket(price) + 1 << " from client\n";
