a98c5ab8337986aaa51a4beb36cefc15eca4be1b
Clint Wyatt <clintwyatt7@gmail.com>
2021-04-27 21:50:15 -0500

    Update ticketServer.cpp

--- server/ticketServer.cpp
+++ server/ticketServer.cpp
@@ -13,10 +13,22 @@ TicketServer::TicketServer(boost::asio::io_service& io_service, const tcp::endpo
 		{
 				if (!ec)
 				{
+					/* the 2 lines of code below also work
+					std::shared_ptr<CustomerSession> pt = std::make_shared<CustomerSession>(tcketBooth, clientSkts, std::move(socket));
+					pt->startSale();
+					*/
+
+					//make a shared_ptr to non-array object
 					std::make_shared<CustomerSession>(tcketBooth, clientSkts, std::move(socket))->startSale();
 				}
 				connect();
 		});
 	}
+
+	/* make_shared
+	*Allocates and constructs an object of type T passing args to its constructor, 
+	* and returns an object of type shared_ptr<T> that owns and stores a pointer to it (with a use count of 1).
+	* This function uses ::new to allocate storage for the object
+	*/
 
 	
