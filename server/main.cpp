c42537dcf8e389b275eac0533a84a4ef404777da
Clint Wyatt <clintwyatt7@gmail.com>
2021-04-27 21:48:43 -0500

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -1,12 +1,24 @@
 #include <boost/asio.hpp>
 #include "ticketServer.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-	boost::asio::io_service io_service;
-	tcp::endpoint endpoint(tcp::v4(), 10000);
-	TicketServer server(io_service, endpoint);
-	std::cout << "Awaiting clients to buy tickets \n";
-	io_service.run();
+	try
+	{
+		if (argc != 2)
+		{
+			std::cout << "usage: <executable> <port number>\n";
+			return 0;
+		}
+		boost::asio::io_service io_service;
+		tcp::endpoint endpoint(tcp::v4(), 10000);
+		TicketServer server(io_service, endpoint);
+		std::cout << "Awaiting clients to buy tiekcts \n";
+		io_service.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << "\n";
+	}
 	return 0;
 }
